37538aafb1835f19667c0d14f2eaf8f2c40e8716
rigelHadushiDev <rigelhadushi4@gmail.com>
2025-03-18 18:22:47 +0100

    Authentication / Authorization Progress

---
+++ Users/USER/job-application-eval/src/main/java/com/example/job_application_eval/config/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package com.example.job_application_eval.config.filters;
+
+import com.sun.istack.internal.NotNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        @NotNull HttpServletRequest request,
+        @NotNull HttpServletResponse response,
+        @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+//        if()
+    }
+}
